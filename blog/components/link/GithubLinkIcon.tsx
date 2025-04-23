e45d9ba3002f4816e86de93090a32a4083a35afb
Andreas Roither <anrodev@proton.me>
2025-04-23 14:25:29 +0200

    style: updating icon sizes

--- blog/components/link/GithubLinkIcon.tsx
+++ blog/components/link/GithubLinkIcon.tsx
@@ -2,25 +2,25 @@ import { siteMetadata } from "@/lib/siteMetadata";
 import Image from "next/image";
 
 export function GithubLinkIcon() {
-	return (
-		<>
-			{siteMetadata.github && (
-				<a
-					href={siteMetadata.siteRepo}
-					target="_blank"
-					rel="noopener noreferrer"
-					aria-label="GitHub Profile"
-					className="hover:text-gray-400"
-				>
-					<Image
-						className="dark:bg-white dark:bg-opacity-10 rounded-full p-1 hover:scale-110 transition-transform duration-150"
-						alt="GitHub"
-						src="/svg/github.svg"
-						width={32}
-						height={32}
-					/>
-				</a>
-			)}
-		</>
-	);
+  return (
+    <>
+      {siteMetadata.github && (
+        <a
+          href={siteMetadata.siteRepo}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub Profile"
+          className="hover:text-gray-400"
+        >
+          <Image
+            className="p-1 transition-transform duration-150 rounded-full dark:bg-white dark:bg-opacity-10 hover:scale-110"
+            alt="GitHub"
+            src="/svg/github.svg"
+            width={32}
+            height={32}
+          />
+        </a>
+      )}
+    </>
+  );
 }
