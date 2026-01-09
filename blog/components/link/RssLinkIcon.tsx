137528232a08d4805eed8628740240e162b3abb8
Andreas Roither <anrodev@proton.me>
2026-01-09 18:37:54 +0100

    fix: rss icon check and incorrect aria label

--- blog/components/link/RssLinkIcon.tsx
+++ blog/components/link/RssLinkIcon.tsx
@@ -4,12 +4,12 @@ import { Rss } from "lucide-react";
 export function RssLinkIcon() {
   return (
     <>
-      {siteMetadata.github && (
+      {siteMetadata.rss && (
         <a
           href={siteMetadata.rss}
           target="_blank"
           rel="noopener noreferrer"
-          aria-label="GitHub Profile"
+          aria-label="RSS Feed"
           className="hover:text-primary"
         >
           <Rss className="w-6 h-6 transition-transform duration-150 sm:h-7 sm:w-7 hover:scale-110" />
