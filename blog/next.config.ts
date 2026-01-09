2e2940128ba83a471369a742e7419f5a9a3bfbc5
Andreas Roither <anrodev@proton.me>
2026-01-09 18:38:43 +0100

    feat: allow localhost image serving in next

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
         search: '',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
     ],
   },
 };
